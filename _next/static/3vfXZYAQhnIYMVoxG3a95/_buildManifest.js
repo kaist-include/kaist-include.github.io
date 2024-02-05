f3ca27637fae56d72fb3ed7577ecb5b70a285190
KimJoongHyun <KimJoongHyun@users.noreply.github.com>
2024-02-05 06:43:24 +0000

    Deploying to gh-pages from @ kaist-include/kaist-include.github.io@a26dbac389d7cd48770cadb2c65e9b6b9c56e957 🚀

--- _next/static/3vfXZYAQhnIYMVoxG3a95/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-f4b821d09876ddf8.js","static/chunks/d7eeaac4-8abb1ed03b931ecc.js","static/chunks/pages/index-630ae768cb7859fd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
