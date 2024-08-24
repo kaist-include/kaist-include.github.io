1b653ef0c55725ef757a5a1cb973d18cb1db46ce
pianojay <pianojay@users.noreply.github.com>
2024-08-24 08:35:42 +0000

    Deploying to gh-pages from @ kaist-include/kaist-include.github.io@14f31fdeabcd2c50673630d164de1414af88bc08 🚀

---
+++ _next/static/Q9j6j-D7XLLWf7vr_dZCd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-f4b821d09876ddf8.js","static/chunks/d7eeaac4-8abb1ed03b931ecc.js","static/chunks/pages/index-ce281f0aa8d3f219.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
