e4076a9cd4cbe575524027624f0bacc98f522ae6
LimePencil <LimePencil@users.noreply.github.com>
2025-07-22 13:17:58 +0000

    Deploying to gh-pages from @ kaist-include/kaist-include.github.io@678a5971e9e726634f806debddbb2be0005f5078 🚀

---
+++ _next/static/0IsXR6Ax4OWU1oE_L_-ac/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-f43df9d0a0474ff2.js","static/chunks/pages/index-a6285b0e0ca23ed0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
