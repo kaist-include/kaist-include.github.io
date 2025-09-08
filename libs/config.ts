a0bcf53e4ee9027640fc535778eb8b19a7d4794d
Jaeyoung Shin <71117066+LimePencil@users.noreply.github.com>
2025-09-08 10:57:54 +0900

    Change recruitment status to not recruiting
    Updated recruitment settings in config.

--- libs/config.ts
+++ libs/config.ts
@@ -3,10 +3,11 @@ const staff = '신재영'
 const staffYear = '2025'
 const staffMail = 'limepencil@kaist.ac.kr'
 const github = 'https://github.com/kaist-include'
-const recruit = ''
+// null if not recruiting
+const recruit = null
 const youtube = 'https://www.youtube.com/@KaistInclude'
 const medium = 'https://medium.com/kaist-include-ai-club'
-const showRecruitmentPopup = true // Set to true when actively recruiting
+const showRecruitmentPopup = false // Set to true when actively recruiting
 const year = new Date().getFullYear()
 
 const Config = {
